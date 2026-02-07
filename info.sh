1cc84313d005f4d064dc96b252fdfb822ab47aa1
plaku <98631804+NTHUB2@users.noreply.github.com>
2026-02-07 16:46:29 +0700

    Update info.sh

--- info.sh
+++ info.sh
@@ -3,6 +3,6 @@ name=Doze GameMode
 version=1.0
 versionCode=1
 author=plaKu
-description=Game-friendly Doze Mode addon. Reduces background drain and standby apps while keeping foreground games unaffected. Users can manage app whitelist manually via whitelist file.
+description=Game-friendly Doze Mode addon. Background apps are restricted while foreground games remain unaffected. Supports manual app whitelisting via config file.
 updateUrl=https://raw.githubusercontent.com/NTHUB2/Magic-Tunner-Add-on/main/version.json
 magicver=5232
