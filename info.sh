0ab20312f8b91fb15ebb15873c40ff6b5799c4f8
plaku <98631804+NTHUB2@users.noreply.github.com>
2026-02-07 15:43:55 +0700

    info.sh

---
+++ info.sh
@@ -0,0 +1,8 @@
+ID=Doze-GameMode-Addon
+name=Doze GameMode
+version=1.0
+versionCode=1
+author=plaKu Extreme
+description=Doze GameMode สำหรับลด background drain และ standby app โดยไม่กระทบ performance เกม
+updateUrl=https://raw.githubusercontent.com/NTHUB2/Magic-Tunner-Add-on/main/version.json
+magicver=5232
